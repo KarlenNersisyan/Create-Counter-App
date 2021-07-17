c9ec6af2ecadb8f4ca22c618eed4a8a1255fea07
KarlNersisyan <karlennersisyan814@gmail.com>
2021-07-18 00:38:29 +0400

    fix(src): correct import file address

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Button from '../src/components/Button/Button';
-import Input from '../src/components/Input/Input';
+import Button from './components/Button/Button';
+import Input from './components/Input/Input';
 import { plusMinusHandler } from './helpers/incrementAndDecrement';
 
 const data = localStorage.getItem('value');
